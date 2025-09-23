000fa5820697d5b768a59820a98ea5773384f37b
QwertyNora <nora.silfver@gmail.com>
2025-09-23 09:21:36 +0200

    add second test

--- TrainingGround.Tests/AgeCalculatorTests.cs
+++ TrainingGround.Tests/AgeCalculatorTests.cs
@@ -14,4 +14,17 @@ public class AgeCalculatorTests
         // assert
         Assert.Equal(50, age);
     }
+
+    [Fact]
+    public void SomeoneBornIn2022_Is0_In2022()
+    {
+        // arrange
+        var calculator = new AgeCalculator();
+
+        // act
+        var age = calculator.GetAge(2022, 2022);
+
+        // assert
+        Assert.Equal(0, age);
+    }
 }
