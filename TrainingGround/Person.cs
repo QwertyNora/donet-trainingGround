4ac5125adee750116493e76e702538cba067270e
QwertyNora <nora.silfver@gmail.com>
2025-09-23 13:43:18 +0200

    cleanup

--- TrainingGround/Person.cs
+++ TrainingGround/Person.cs
@@ -1,26 +1,22 @@
 
 public class Person
 {
-    // Name property, private set
     public string? Name { get; private set; }
+    public int BirthYear { get; private set; }
+    public double LengthInMeters { get; private set; }
 
-    //Person Name constructor
     public Person(string name)
     {
         this.Name = name;
     }
 
-    // BirthYear property, private set
-    public int BirthYear { get; private set; }
-
-    // Person BithYear constructor
     public Person(int birthYear)
     {
         this.BirthYear = birthYear;
     }
-    public double LengthInMeters;
 
-    // Constructor to initialize both name, birth year and length
+    public Person() { }
+
     public Person(string name, int birthYear, double lengthInMeters)
     {
         this.Name = name;
@@ -28,10 +24,6 @@ public class Person
         this.LengthInMeters = lengthInMeters;
     }
 
-    //Constructor that makes Null possible
-    public Person() { }
-
-    // Get Persons age method
     public int GetAge(int currentYear)
     {
         return currentYear - this.BirthYear;
