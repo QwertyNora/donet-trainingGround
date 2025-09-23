c968ffc892d46e72f97f211581d02e7a3331c57b
QwertyNora <nora.silfver@gmail.com>
2025-09-23 09:33:55 +0200

    declare new private field instance of AgeCalculator for tests

--- TrainingGround.Tests/AgeCalculatorTests.cs
+++ TrainingGround.Tests/AgeCalculatorTests.cs
@@ -2,14 +2,21 @@
 
 public class AgeCalculatorTests
 {
+    private AgeCalculator _calculator;
+
+    public AgeCalculatorTests()
+    {
+        _calculator = new AgeCalculator();
+    }
+
     [Fact]
     public void SomeoneBornIn1972_Is50_In2022()
     {
         // arrange 
-        var calculator = new AgeCalculator();
+        // var calculator = new AgeCalculator();
 
         // act 
-        var age = calculator.GetAge(1972, 2022);
+        var age = _calculator.GetAge(1972, 2022);
 
         // assert
         Assert.Equal(50, age);
@@ -19,10 +26,10 @@ public class AgeCalculatorTests
     public void SomeoneBornIn2022_Is0_In2022()
     {
         // arrange
-        var calculator = new AgeCalculator();
+        // var calculator = new AgeCalculator();
 
         // act
-        var age = calculator.GetAge(2022, 2022);
+        var age = _calculator.GetAge(2022, 2022);
 
         // assert
         Assert.Equal(0, age);
