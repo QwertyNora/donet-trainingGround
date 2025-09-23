b7bcc65331ebd14532dc649bb881ba1010aed99d
QwertyNora <nora.silfver@gmail.com>
2025-09-23 10:26:18 +0200

    set Name field to private, add GetName and SetName methods

--- TrainingGround/Person.cs
+++ TrainingGround/Person.cs
@@ -1,12 +1,28 @@
 public class Person
 {
-    public string Name;
-    public int BirthYear;
-    public double LengthInMeters;
+    private string? Name;
 
-    public Person() { }
+    //Person name constructor
     public Person(string name)
     {
         this.Name = name;
     }
+
+    public string GetName()
+    {
+        return this.Name;
+    }
+
+    public string SetName(string nameToSet)
+    {
+        if (nameToSet > 5)
+        {
+            this.Name = nameToSet;
+        }
+    }
+    public int BirthYear;
+    public double LengthInMeters;
+
+    //Constructor that makes Null possible
+    public Person() { }
 }
