56477d4530b54b9bc46731ed5cdd5da41484fdf3
QwertyNora <nora.silfver@gmail.com>
2025-09-23 09:13:37 +0200

    write first test draft

--- TrainingGround.Tests/AgeCalculatorTests.cs
+++ TrainingGround.Tests/AgeCalculatorTests.cs
@@ -5,6 +5,13 @@ public class AgeCalculatorTests
     [Fact]
     public void SomeoneBornIn1972_Is50_In2022()
     {
+        // arrange 
+        var calculator = new AgeCalculator();
 
+        // act 
+        var age = calculator.GetAge(1972, 2022);
+
+        // assert
+        Assert.Equal(50, age);
     }
 }
