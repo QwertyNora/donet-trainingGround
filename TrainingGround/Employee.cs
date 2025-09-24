b9eb90c2b4209c7ca2d2124237bc07a0b343e226
QwertyNora <nora.silfver@gmail.com>
2025-09-24 13:13:23 +0200

    Add EmployeeId property and constructor to Employee class

--- TrainingGround/Employee.cs
+++ TrainingGround/Employee.cs
@@ -2,5 +2,11 @@ namespace TrainingGround;
 
 public class Employee : Person
 {
+    public string EmployeeId { get; set; }
 
+    public Employee() { }
+    public Employee(string name, string employeeID) : base(name)
+    {
+        this.EmployeeId = employeeID;
+    }
 }
