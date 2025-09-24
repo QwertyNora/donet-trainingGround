1904851e66650ef37eee5608aa522f240c135168
QwertyNora <nora.silfver@gmail.com>
2025-09-24 13:13:02 +0200

    Remove commented-out Console.WriteLine statements in TypesTests class

--- TrainingGround.Tests/TypesTests.cs
+++ TrainingGround.Tests/TypesTests.cs
@@ -42,42 +42,29 @@ public class TypesTests
         int x = 10;
         int y = 20;
 
-        Console.WriteLine("init x: " + x);
-        Console.WriteLine("init y: " + y);
+        // Console.WriteLine("init x: " + x);
+        // Console.WriteLine("init y: " + y);
 
         x = y;
 
-        Console.WriteLine("x after x = y    x: " + x);
-        Console.WriteLine("y after x = y    y: " + y);
+        // Console.WriteLine("x after x = y    x: " + x);
+        // Console.WriteLine("y after x = y    y: " + y);
 
         x++;
 
-        Console.WriteLine("x after incremet   x: " + x);
-        Console.WriteLine("y after incremet   y: " + y);
+        // Console.WriteLine("x after incremet   x: " + x);
+        // Console.WriteLine("y after incremet   y: " + y);
     }
 
     [Fact]
     public void ReferenceTypes_Test()
     {
-
-
-
         var p1 = new Person("Anna");
         var p2 = new Person("Oscar");
 
-        Console.WriteLine($"P1 name: {p1.Name}");
-        Console.WriteLine($"P2 name: {p2.Name}");
-
         p1 = p2;
 
-        Console.WriteLine($"P1 name after p1 = p2: {p1.Name}");
-        Console.WriteLine($"P2 name after p1 = p2: {p2.Name}");
-
         // p2.Name = "Freddy";
-
-        Console.WriteLine($"P1 name: {p1.Name}");
-        Console.WriteLine($"P2 name: {p2.Name}");
-
 
     }
 }
