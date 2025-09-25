fd12dd32c7aa591bad4dd0fbfd87a76168614957
QwertyNora <nora.silfver@gmail.com>
2025-09-25 09:24:13 +0200

    Add AgeCategory enum and GetAgeCategory method to AgeCalculator

--- TrainingGround/AgeCalculator.cs
+++ TrainingGround/AgeCalculator.cs
@@ -1,9 +1,20 @@
 ﻿namespace TrainingGround;
 
+public enum AgeCategory
+{
+    Kid
+}
+
 public class AgeCalculator
 {
+
     public static int GetAge(int birthYear, int currentYear)
     {
         return currentYear - birthYear;
+    }
+
+    public static AgeCategory GetAgeCategory(Person person, int currentYear)
+    {
+        return GetAgeCategory.Kid;
     }
 }
