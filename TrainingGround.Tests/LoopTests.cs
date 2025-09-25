6e3bc0dc4f7b08ce2968e2433f2cab091a951ede
QwertyNora <nora.silfver@gmail.com>
2025-09-25 14:42:18 +0200

    Add WhileLoop test to LoopTests for validating loop iteration

--- TrainingGround.Tests/LoopTests.cs
+++ TrainingGround.Tests/LoopTests.cs
@@ -2,5 +2,25 @@ namespace TrainingGround;
 
 public class LoopTests
 {
+    [Fact]
+    public void WhileLoop()
+    {
+        // arrange
+        var ints = new int[] { 1, 2, 3, 4, 5 };
 
+        // act
+        var i = 0;
+        while (i < ints.Length)
+        {
+            var currentValueInTheLoop = ints[i];
+
+            Console.WriteLine($"i is now '{i}'");
+            Console.WriteLine($"currentValueInTheLoop is now '{currentValueInTheLoop}'");
+
+            // assert
+            Assert.Equal(i + 1, currentValueInTheLoop);
+
+            i = i + 1;
+        }
+    }
 }
