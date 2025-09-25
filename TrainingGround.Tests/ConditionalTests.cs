2276c189d3d130a69340b8abf32de838da2e66d3
QwertyNora <nora.silfver@gmail.com>
2025-09-25 09:24:06 +0200

    Add ConditionalTests to verify age category logic

---
+++ TrainingGround.Tests/ConditionalTests.cs
@@ -0,0 +1,18 @@
+namespace TrainingGround.Tests;
+
+public class ConditionalTests
+{
+
+    [Fact]
+    public void ShouldBeKid_WhenYoungerThan18()
+    {
+        // arrange
+        var p = new Person(2005);
+
+        // act
+        var category = AgeCalculator.GetAgeCategory(p, 2022);
+
+        // assert
+        Assert.Equal(AgeCategory.Kid, category);
+    }
+}
