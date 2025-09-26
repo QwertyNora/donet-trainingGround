3ecc54468ba76a00d77902a52b1c60dd83299e9f
QwertyNora <nora.silfver@gmail.com>
2025-09-26 08:41:19 +0200

    Attemt to divide by zero, trigger DivideByZeroException

--- TrainingGround.Tests/ExceptionsTests.cs
+++ TrainingGround.Tests/ExceptionsTests.cs
@@ -2,5 +2,13 @@ namespace TrainingGround.Tests;
 
 public class ExceptionsTests
 {
+    [Fact]
+    public void NotHandlingAnException()
+    {
+        // arrange
+        var i = 1;
 
+        // act
+        var result = 14 / (i - 1);
+    }
 }
